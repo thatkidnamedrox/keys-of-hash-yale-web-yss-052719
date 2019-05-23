a2e7a72eaf1bd01af3469230d6aefdd923ea3470
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-23 20:25:19 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,21 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    args = []
+    if !arguments.is_a?(Array)
+      args = [arguments]
+    else
+      args = arguments
+    end
+    #puts args.inspect
+    selection = nil
+    args.each do |arg|
+      puts arg
+      selection = self.select {|k,v| v == arg}
+      puts selection.inspect
+    end
+    puts keys
+    keys = selection.keys
+
   end
 end
