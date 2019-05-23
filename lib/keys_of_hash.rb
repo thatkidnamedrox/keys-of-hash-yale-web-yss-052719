17bdc58b88a14dfc29ab8e1831fcfce4fae0964c
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-23 20:33:08 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,19 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    args = []
+    if !arguments.is_a?(Array)
+      args = [arguments]
+    else
+      args = arguments
+    end
+
+    result = {}
+    args.each do |arg|
+      selection = self.select {|k,v| v == arg}
+      result = result.merge(selection)
+    end
+    result.keys
+
   end
 end
